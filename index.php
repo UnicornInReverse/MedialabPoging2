daf5a56e487f2d3f0b8c232e684df5f2cdd21358
Maaike Koolmees <0911174@hr.nl>
2018-04-11 20:44:13 +0200

    Git negeer

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+    <meta charset="utf-8">
+    <title>Flow.ai Web Client</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        window.__flowai_webclient_autoOpen = true
+    </script>
+</head>
+<body>
+<script src="https://widget.flow.ai/w/YWM3YzJjMGYtYWQxMi00M2E5LTk3MzctZGQ1MDFhNWNjMTEyfDZmMDdjMTA3LThkNzItNDQwMS1hM2VkLTc0NDE0MWU4YTY0OA==/flow-webclient-1.1.2.min.js?r=1523471016485"></script>
+</body>
+</html>
