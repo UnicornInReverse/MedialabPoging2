bd65355024e7f00137fc20ab7d46a6c695b58d0b
Maaike Koolmees <0911174@hr.nl>
2018-04-11 21:33:38 +0200

    ALSJEBLIEFT

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php
+require __DIR__ . '/speech.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>HUB - Jouw contact met mantelzorgers</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="lib/css/style.css">
     <script>
         window.__flowai_webclient_autoOpen = true
     </script>
 </head>
 <body>
 <script src="https://widget.flow.ai/w/YWM3YzJjMGYtYWQxMi00M2E5LTk3MzctZGQ1MDFhNWNjMTEyfDZmMDdjMTA3LThkNzItNDQwMS1hM2VkLTc0NDE0MWU4YTY0OA==/flow-webclient-1.1.2.min.js?r=1523471016485"></script>
+<script src="main.js"></script>
 </body>
 </html>
 <body>
 
-<button type="button">Neem op</button>
+<button class=button type="button">Neem op</button>
 
---
+++ speech.php
@@ -0,0 +1,36 @@
+<?php
+# Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
+# Imports the Google Cloud client library
+use Google\Cloud\Speech\SpeechClient;
+
+# Your Google Cloud Platform project ID
+$projectId = 'medialab-197412 ';
+
+$config = [
+    'projectId' => $projectId,
+];
+
+
+# Instantiates a client
+$speech = new SpeechClient([
+    'projectId' => $projectId,
+    'languageCode' => 'en-US',
+]);
+
+# The name of the audio file to transcribe
+$fileName = __DIR__ . '/resources/audio.raw';
+
+# The audio file's encoding and sample rate
+$options = [
+    'encoding' => 'LINEAR16',
+    'sampleRateHertz' => 16000,
+];
+
+# Detects speech in the audio file
+$results = $speech->recognize(fopen($fileName, 'r'), $options);
+
+foreach ($results as $result) {
+    echo 'Transcription: ' . $result->alternatives()[0]['transcript'] . PHP_EOL;
+}
