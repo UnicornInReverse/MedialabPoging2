f263c7f1e2c600a501c81a0d2a8fa39270d877ee
Tessa <0909922@hr.nl>
2018-04-11 20:41:07 +0200

    basic set up

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HUB - Jouw contact met mantelzorgers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        window.__flowai_webclient_autoOpen = true
+    </script>
+
+</head>
+<body>
+
+<button type="button">Neem op</button>
+
+
+<script src="https://widget.flow.ai/w/YWM3YzJjMGYtYWQxMi00M2E5LTk3MzctZGQ1MDFhNWNjMTEyfDZmMDdjMTA3LThkNzItNDQwMS1hM2VkLTc0NDE0MWU4YTY0OA==/flow-webclient-1.1.2.min.js%22%3E"> </script>
+
+
+</body>
+</html>
